2fb6c13e16c1e9deca90499f9fc3851b4ec96684
Eduardo Sacco <saccoeduardo@gmail.com>
2021-08-27 02:49:38 -0300

    Slight UI improvements in chat UI.

--- FinancialChat/FinancialChat/wwwroot/js/chat.js
+++ FinancialChat/FinancialChat/wwwroot/js/chat.js
@@ -6,12 +6,16 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
+
+    var li = document.createElement("li");
+    var userName = document.createElement("b");
+    userName.textContent = `${user}: `;
+    document.getElementById("messagesList").appendChild(li);
+    li.appendChild(userName);
+    li.appendChild(document.createTextNode(`${message}`));
 });
 
 connection.start().then(function () {
