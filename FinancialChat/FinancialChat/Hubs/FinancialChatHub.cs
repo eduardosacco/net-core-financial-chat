99a44b689dd60de88145595154ad08920c8a76e0
Eduardo Sacco <saccoeduardo@gmail.com>
2021-08-29 02:23:24 -0300

    Use username identity claims as user name for chat

--- FinancialChat/FinancialChat/Hubs/FinancialChatHub.cs
+++ FinancialChat/FinancialChat/Hubs/FinancialChatHub.cs
@@ -1,26 +1,36 @@
 ﻿using FinancialChat.Commands;
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using FinancialChat.Areas.Identity.Data;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace FinancialChat.Hubs
 {
     public class FinancialChatHub : Hub
     {
+        private readonly IHttpContextAccessor httpContextAccessor;
         private readonly IStockBotCommandSender stockBotCommandSender;
 
-        public FinancialChatHub(IStockBotCommandSender stockBotCommandSender)
+        public FinancialChatHub(IHttpContextAccessor httpContextAccessor,
+            IStockBotCommandSender stockBotCommandSender)
         {
+            this.httpContextAccessor = httpContextAccessor;
             this.stockBotCommandSender = stockBotCommandSender;
         }
 
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string message)
         {
             if(await HandleCommand(message))
             {
                 return;
             }
 
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            var email = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+            var userName = email.Split('@')[0];
+
+            await Clients.All.SendAsync("ReceiveMessage", userName, message);
         }
 
         private async Task<bool> HandleCommand(string message)
