7a38d0bc5422c32cf714b8ab4e0afc4625483659
Eduardo Sacco <saccoeduardo@gmail.com>
2021-08-28 23:53:39 -0300

    Order newer messages first. Limit chat to 50.

--- FinancialChat/FinancialChat/wwwroot/js/chat.js
+++ FinancialChat/FinancialChat/wwwroot/js/chat.js
@@ -10,15 +10,26 @@ connection.on("ReceiveMessage", function (user, message) {
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
 
+    // Messages already arrive in order by time
     var date = new Date();
     var timestamp = date.toLocaleString('es-AR');
+
+    // remove messages if over 50 elements
+    var messageList = document.getElementById("messagesList");
+    if (messageList.childElementCount > 50) {
+        messageList.lastElementChild.remove();
+    }
+
     var li = document.createElement("li");
     var userName = document.createElement("b");
     userName.textContent = `${user}: `;
-    document.getElementById("messagesList").appendChild(li);
     li.appendChild(document.createTextNode(`[${timestamp}] - `));
     li.appendChild(userName);
     li.appendChild(document.createTextNode(`${message}`));
+
+    // Prepend so newer messages appear first
+    messageList.prepend(li);
+
 });
 
 connection.start().then(function () {
